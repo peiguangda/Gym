ea8a9b98f622b87d4a3d9b2e2c69935941a1256a
Quang Dai <peiguangda@gmail.com>
2017-11-18 23:17:37 +0700

    home layout

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <title>Home</title>
+  <!-- Latest compiled and minified CSS & JS -->
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/home.css">
+  <link href="/maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <script src="/code.jquery.com/jquery.js"></script>
+</head>
+<body>
+<!--- - - - - - - -navbar - - - - - - -->
+    @include('partials.navbar_home')
+<!--- - - - - - - -navbar - - - - - - -->
+ 
+
+
+<!-- - - - - - - -  carousel- - - - - - - - - -->
+    @include('partials.carousel')
+<!-- - - - - - - - - carousel - - - - - - -  -->
+
+
+<!-- - - - - - - - - show - - - - - - -  -->
+    @include('partials.list_programs_home')
+<!-- - - - - - - - - show - - - - - - -  -->
+
+
+
+<!-- - - - - - - - - footer - - - - - - -  -->
+    @include('partials.footer')
+<!-- - - - - - - - - footer - - - - - - -  -->
+
+
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="/js/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="/js/bootstrap.min.js"></script>
+  <script src="/js/home.js"></script>
+</body>
+
+</html>
+
+
---
+++ resources/views/partials/navbar_home.blade.php
@@ -0,0 +1,191 @@
+<div class="container">
+  <nav class="navbar navbar-inverse navtop">
+    <div class="navbar-header">
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="{{route('home')}}"> <strong>Gym</strong> </a>
+    </div>
+    
+    <div class="collapse navbar-collapse js-navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li class="dropdown mega-dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">XXX <span class="caret"></span></a>              
+                <ul class="dropdown-menu mega-dropdown-menu">
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">XXX</li>                            
+                            <div id="menCollection" class="carousel slide" data-ride="carousel">
+                              <div class="carousel-inner">
+                                <div class="item active">
+                                    <a href="#"><img src="http://placehold.it/254x150/ff3546/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 1"></a>
+                                    <h4><small>trà sữa x</small></h4>                                        
+                                    <button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>       
+                                </div><!-- End Item -->
+                                <div class="item">
+                                    <a href="#"><img src="http://placehold.it/254x150/3498db/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 2"></a>
+                                    <h4><small>trà sữa x</small></h4>                                        
+                                    <button class="btn btn-primary" type="button">9,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>        
+                                </div><!-- End Item -->
+                                <div class="item">
+                                    <a href="#"><img src="http://placehold.it/254x150/2ecc71/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 3"></a>
+                                    <h4><small>trà sữa x</small></h4>                                        
+                                    <button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>      
+                                </div><!-- End Item -->                                
+                              </div><!-- End Carousel Inner -->
+                              <!-- Controls -->
+                              <a class="left carousel-control" href="#menCollection" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="right carousel-control" href="#menCollection" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div><!-- /.carousel -->
+                            <li class="divider"></li>
+                            <li><a href="#">View all Collection <span class="glyphicon glyphicon-chevron-right pull-right"></span></a></li>
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Features</li>
+                            <li><a href="#">Auto Carousel</a></li>
+                            <li><a href="#">Carousel Control</a></li>
+                            <li><a href="#">Left & Right Navigation</a></li>
+                            <li><a href="#">Four Columns Grid</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Fonts</li>
+                            <li><a href="#">Glyphicon</a></li>
+                            <li><a href="#">Google Fonts</a></li>
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Plus</li>
+                            <li><a href="#">Navbar Inverse</a></li>
+                            <li><a href="#">Pull Right Elements</a></li>
+                            <li><a href="#">Coloured Headers</a></li>                            
+                            <li><a href="#">Primary Buttons & Default</a></li>                          
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Much more</li>
+                            <li><a href="#">Easy to Customize</a></li>
+                            <li><a href="#">Calls to action</a></li>
+                            <li><a href="#">Custom Fonts</a></li>
+                            <li><a href="#">Slide down on Hover</a></li>                         
+                        </ul>
+                    </li>
+                </ul>               
+            </li>
+            <li class="dropdown mega-dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Category <span class="caret"></span></a>                
+                <ul class="dropdown-menu mega-dropdown-menu">
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Trà sữa</li>
+                            <li><a href="#">x</a></li>
+                            <li><a href="#">x</a></li>
+                            <li><a href="#">x</a></li>
+                            <li><a href="#">x</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Shit</li>
+                            <li><a href="#">y</a></li>
+                            <li><a href="#">y</a></li>
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Cà phê</li>
+                            <li><a href="#">Navbar Inverse</a></li>
+                            <li><a href="#">Pull Right Elements</a></li>
+                            <li><a href="#">Coloured Headers</a></li>                            
+                            <li><a href="#">Primary Buttons & Default</a></li>                          
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Sữa tươi</li>
+                            <li><a href="#">Easy to Customize</a></li>
+                            <li><a href="#">Calls to action</a></li>
+                            <li><a href="#">Custom Fonts</a></li>
+                            <li><a href="#">Slide down on Hover</a></li>                         
+                        </ul>
+                    </li>
+                    <li class="col-sm-3">
+                        <ul>
+                            <li class="dropdown-header">Sữa tươi</li>
+                            <li><a href="#">Easy to Customize</a></li>
+                            <li><a href="#">Calls to action</a></li>
+                            <li><a href="#">Custom Fonts</a></li>
+                            <li><a href="#">Slide down on Hover</a></li>                         
+                        </ul>
+                    </li>
+                </ul>               
+            </li>
+            <li><a href="#">Store locator</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">My account <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown mega-dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cart <span class="badge">10</span></a> 
+            <ul class="dropdown-menu mega-dropdown-menu" id = cart>
+                <table id="cart" class="table table-hover table-condensed">
+                    <thead>
+                        <tr>
+                            <th style="width:50%">Product</th>
+                            <th style="width:10%">Price</th>
+                            <th style="width:8%">Quantity</th>
+                            <th style="width:22%" class="text-center">Dicount</th>
+                            <th style="width:10%"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="list-product">
+                    <!-- foreach -->
+                        <tr>
+                            <td data-th="Product">
+                                <div class="row">
+                                    <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+                                    <div class="col-sm-10"><h4 class="nomargin"></h4><p>...............</p></div>
+                                </div>
+                            </td>
+                            <td data-th="Price">.......... VND</td>
+                            <td data-th="Quantity"><input type="number" class="form-control text-center" value="1"></td>
+                            <td data-th="Subtotal" class="text-center">...........</td>
+                            <td class="actions" data-th=""><button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button><button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button></td> 
+                        </tr>
+                      <!-- endforeach -->
+                    </tbody>
+                    <tfoot>
+                        <tr class="visible-xs">
+                            <td class="text-center"><strong>Total 1.99</strong></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" class="hidden-xs"></td>
+                            <td class="hidden-xs text-center"><strong  id="total">.....</strong></td>
+                            <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </ul>               
+        </li>
+      </ul>
+    </div><!-- /.nav-collapse -->
+  </nav>
+</div>
